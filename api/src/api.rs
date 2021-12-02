5ba537b0ed798cc9ee09dab79be93cd647051e18
Avery Whitehead <averywhthead@gmail.com>
2021-12-02 10:17:27 -0800

    Use rocket_cors instead of fairing implementation to fix CORS problems during POST requests

--- api/src/api.rs
+++ api/src/api.rs
@@ -3,14 +3,9 @@ use diesel::prelude::*;
 use crate::db_pool::DbConn;
 use crate::models::{Note, NewNote};
 use crate::schema::notes::dsl::*;
-use rocket::{Request, Response};
-use rocket::fairing::{Fairing, Info, Kind};
 use rocket::get;
-use rocket::http::Header;
 use rocket_contrib::json::Json;
 use std::ops::Deref;
-
-pub struct CORS;
 
 #[get("/")]
 pub fn get_all(conn: DbConn) -> QueryResult<Json<Vec<Note>>> {
@@ -31,21 +26,5 @@ impl Deref for DbConn {
     type Target = PgConnection;
     fn deref(&self) -> &Self::Target {
         &self.0
-    }
-}
-
-impl Fairing for CORS {
-    fn info(&self) -> Info {
-        Info {
-            name: "Add CORS headers to responses",
-            kind: Kind::Response
-        }
-    }
-
-    fn on_response(&self, _request: &Request, response: &mut Response) {
-        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
-        response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"));
-        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
-        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
     }
 }
--- api/src/main.rs
+++ api/src/main.rs
@@ -5,6 +5,9 @@ extern crate rocket_contrib;
 
 use anyhow::Result;
 use dotenv::dotenv;
+use rocket::http::Method;
+use rocket::routes;
+use rocket_cors::{AllowedHeaders, AllowedOrigins};
 use std::env;
 
 pub mod db_pool;
@@ -15,10 +18,20 @@ pub mod api;
 fn main() -> Result<()> {
     dotenv()?;
     let database_url = env::var("DATABASE_URL")?;
+    let cors = rocket_cors::CorsOptions {
+        allowed_origins: AllowedOrigins::all(),
+        allowed_methods: vec![Method::Get, Method::Post].into_iter().map(From::from).collect(),
+        allowed_headers: AllowedHeaders::all(),
+        allow_credentials: true,
+        ..Default::default()
+    }
+    .to_cors()?;
     rocket::ignite()
         .manage(db_pool::init(&database_url))
         .mount("/api/notes", routes![api::get_all, api::create])
-        .attach(api::CORS)
+        .mount("/api/notes", rocket_cors::catch_all_options_routes())
+        .attach(cors.clone())
+        .manage(cors)
         .launch();
     Ok(())
 }
